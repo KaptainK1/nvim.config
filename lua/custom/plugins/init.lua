f15332ad1a0577efc27b27743fe9a12daf492088
Dylan Hoffman <jkioicefire@gmail.com>
2025-01-26 16:17:08 -0600

    harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -217,4 +217,41 @@ return {
       },
     },
   },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+      vim.keymap.set('n', '<leader>la', function()
+        harpoon:list():add()
+      end)
+      vim.keymap.set('n', '<leader>ll', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set('n', '<leader>lh', function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set('n', '<leader>lj', function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set('n', '<leader>lk', function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set('n', '<leader>l;', function()
+        harpoon:list():select(4)
+      end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set('n', '<C-B>', function()
+        harpoon:list():prev()
+      end)
+      vim.keymap.set('n', '<C-N>', function()
+        harpoon:list():next()
+      end)
+    end,
+  },
 }
